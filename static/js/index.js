6f303592d73aefcc4bbcbf1e6aa9cb194128a113
Grace2614 <80277169+Grace2614@users.noreply.github.com>
2021-03-31 01:31:45 -0500

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -57,7 +57,7 @@ function control(){
 	
 	var datos = new Array();
 	for (var i=0; i<=10; i++){
-		datos[i] = message.payloadString;
+		datos[i] = +message.payloadString;
 	setTimeout(message.payloadString,3000);
 		//UpdateElement(message.payloadString) 
 	}
